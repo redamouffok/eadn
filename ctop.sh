1286210cd01b39028ca5316887c4d616df59ea92
reda <rdmouffok@gmail.com>
2025-09-29 10:10:35 +0100

    fix all

---
+++ ctop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get install ca-certificates curl gnupg lsb-release
+curl -fsSL https://azlux.fr/repo.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/azlux-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/azlux-archive-keyring.gpg] http://packages.azlux.fr/debian \
+  $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/azlux.list >/dev/null
+sudo apt-get update
+sudo apt-get install docker-ctop
